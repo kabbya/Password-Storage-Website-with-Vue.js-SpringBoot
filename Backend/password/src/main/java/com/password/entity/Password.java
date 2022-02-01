cbe8f796ff9afc43eaa30ac7b8a3dc19855d3eb5
kabbya <kabbya.kantam@gmail.com>
2022-02-01 19:48:01 +0600

    Created database table

---
+++ Backend/password/src/main/java/com/password/entity/Password.java
@@ -0,0 +1,84 @@
+package com.password.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+@Entity
+@Table (name = "Passwords")
+public class Password {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long pId;
+	private String siteName;
+	private String sitePassword;
+	private String description;
+	private boolean published;
+
+	@ManyToOne
+	private User user;
+
+	public Password() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getpId() {
+		return pId;
+	}
+
+	public void setpId(long pId) {
+		this.pId = pId;
+	}
+
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+
+	public String getSitePassword() {
+		return sitePassword;
+	}
+
+	public void setSitePassword(String sitePassword) {
+		this.sitePassword = sitePassword;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+	public void setPublished(boolean published) {
+		this.published = published;
+	}
+	
+	@JsonBackReference
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
